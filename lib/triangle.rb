67bc962254bf8eac060f4204cb4c3acb0f6788eb
Daniel-Mack <daniel.mack@powerhrg.com>
2018-05-29 11:51:06 -0400

    Completed lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    # binding.pry
+    if [@a,@b,@c].min <= 0 ||  @a + @b <= @c || @b + @c <= @a || @a + @c <= @b
+      raise TriangleError
+    end
+    if @a == @b && @a == @c && @b == @c
+      return :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      return :isosceles
+    else
+      return :scalene
+  end
+end
+
+  class TriangleError < StandardError
+    def message
+      "Triangles with no size are illegal"
+    end
+  end
 end
